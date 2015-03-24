1264c6531db8de52506268935849307dba76213f
Reyvilo <margerit.olivier@gmail.com>
2015-03-24 11:03:23 +0100

    Controllers

--- src/GSB_Frais_MVC/GSBFraisBundle/Controller/ComptableController.php
+++ src/GSB_Frais_MVC/GSBFraisBundle/Controller/ComptableController.php
@@ -1,13 +1,22 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 24/03/15
+ * Time: 10:57
+ */
 
-namespace GSB_Frais_MVC\GSBFraisBundle\Controller;
+namespace GSB_Frais_MVC\GSBFraisBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+
+class LoginController extends Controller
 {
+
     public function indexAction($name)
     {
         return $this->render('GSBFraisBundle:Default:index.html.twig', array('name' => $name));
     }
+
 }
---
+++ src/GSB_Frais_MVC/GSBFraisBundle/Controller/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 24/03/15
+ * Time: 10:57
+ */
+
+namespace GSB__Frais_MVC\GSBFraisBundle\Tests\Controller;
+
+
+class LoginController {
+
+}
