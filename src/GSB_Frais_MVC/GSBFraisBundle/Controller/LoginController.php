603a2b48d864feb6c8da4dac6faea223ea7d2f45
Alexisvarnieu <alexis.varnieu@epsi.fr>
2015-03-25 13:52:23 +0100

    lol

--- src/GSB_Frais_MVC/GSBFraisBundle/Controller/LoginController.php
+++ src/GSB_Frais_MVC/GSBFraisBundle/Controller/LoginController.php
@@ -18,5 +18,28 @@ class LoginController extends Controller
     {
         return $this->render('GSBFraisBundle:Default:index.html.twig', array('name' => $name));
     }
-
+    public function validerconnexionAction(){
+        $session= $this->get('request')->getSession();
+        $request = $this->get('request');
+        $login =  $request->request->get('login');
+        $mdp = $request->request->get('mdp');
+        //$pdo = PdoGsb::getPdoGsb();
+        $pdo = $this->get('pg_gsb_frais.pdo');
+        $visiteur = $pdo->getInfosVisiteur($login,$mdp);
+        if(!is_array($visiteur)){
+            return $this->render('GSBFraisBundle:Home:connexion.html.twig',array(
+                'message'=>'Erreur de login ou de mot de passe '));
+        }
+        else{
+            $session->set('id',$visiteur['id']);
+            $session->set('nom',$visiteur['nom']);
+            $session->set('prenom',$visiteur['prenom']);
+            return $this->render('PgGsbFraisBundle::accueil.html.twig');
+        }
+    }
+    public function deconnexionAction(){
+        $session= $this->get('request')->getSession();
+        $session->clear();
+        return $this->render('GSBFraisBundle:Home:connexion.html.twig');
+    }
 }
